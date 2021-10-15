441bbf4e6e633990fd9467e3af984805bed08cd9
pak <rupaklamadev@gmail.com>
2021-10-15 00:04:50 -0400

    created components & styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/home/Home';
+import About from './pages/about/About';
+import NotFound from './pages/not-found/NotFound';
+
 function App() {
   return (
-    <div>
-      <h1>hello travel website</h1>
-      <p>Rupak Lama</p>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/about' component={About} />
+
+        {/* must be at the end of the list */}
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+const Header = () => {
+  const [state] = useState({
+    video: './assets/videos/header.mp4',
+  });
+
+  const { video } = state;
+
+  return (
+    <div className='header'>
+      <div className='header__video'>
+        <video src={video} autoPlay loop muted></video>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
