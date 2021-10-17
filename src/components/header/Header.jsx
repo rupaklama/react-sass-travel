7f0c10322f4bc05b2493c91a4af961e947e66f64
pak <rupaklamadev@gmail.com>
2021-10-16 23:38:54 -0400

    styled button

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,28 @@
 import { useState } from 'react';
 
-const Header = () => {
+const Header = ({ children, heading, paragraph }) => {
   const [state] = useState({
     video: './assets/videos/header.mp4',
+    poster: './assets/images/screen.png',
   });
 
-  const { video } = state;
+  const { video, poster } = state;
 
   return (
     <div className='header'>
-      <div className='header__video'>
-        <video src={video} autoPlay loop muted></video>
+      <div className='header-video'>
+        <video src={video} autoPlay loop muted poster={poster}></video>
+      </div>
+      <div className='header-contents'>
+        <div className='container'>
+          <div className='header-contents-text'>
+            <div className='header-contents-text-child'>
+              <h1 className='header-contents-text-child-h1'>{heading}</h1>
+              <p className='header-contents-text-child-p'>{paragraph}</p>
+              <div className='header-contents-text-child-link'>{children}</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,21 @@
+import { useState } from 'react';
+
 import Header from '../../components/header/Header';
 
 const Home = () => {
+  const [state] = useState({
+    heading: 'We are travel friends',
+    paragraph:
+      'Come & join us! We travel the most famous and beautiful places in the world',
+  });
+
+  const { heading, paragraph } = state;
+
   return (
     <>
-      <Header />
+      <Header heading={heading} paragraph={paragraph}>
+        <button className='btn-default'>Get Started</button>
+      </Header>
     </>
   );
 };
