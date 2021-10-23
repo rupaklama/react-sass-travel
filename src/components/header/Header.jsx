9e5afc197ca0d111822934ed82250de2d95021e9
pak <rupaklamadev@gmail.com>
2021-10-22 22:59:39 -0400

    updated styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,12 +4,19 @@ const Header = ({ children, heading, paragraph }) => {
   const [state] = useState({
     video: './assets/videos/header.mp4',
     poster: './assets/images/screen.png',
+    logo: './assets/images/logo.png',
   });
 
-  const { video, poster } = state;
+  const { video, poster, logo } = state;
 
   return (
     <div className='header'>
+      <div className='logo-container'>
+        <div className='header-logo'>
+          <img src={logo} alt='logo' />
+        </div>
+      </div>
+
       <div className='header-video'>
         <video src={video} autoPlay loop muted poster={poster}></video>
       </div>
